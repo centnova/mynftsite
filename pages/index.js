318bcde5437dafc36cdae0302335c8960c26963e
john <microrey20@gmail.com>
2022-04-20 13:05:25 -0700

    Implement whitelist for index.js

--- pages/index.js
+++ pages/index.js
@@ -25,9 +25,32 @@ const Home = props => {
   )
 }
 
-Home.getInitialProps = async ({ req, res }) => {
-  res.statusCode = 403
-  res.end(`Not allowed`)
+Home.getInitialProps = async ({ req, res }) => {// Disallow access by default
+  let shouldDisallowAccess = true
+
+  // Specify IP's that you want to whitelist. This could be an office/VPN IP address.
+  const allowedIps = ['37.6.11.79', '::ffff:127.0.0.1']
+
+  // Read the request objects headers to find our who's trying to access this page
+  const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress
+
+  // This if statement is only valid if the app is deployed through Now. It gets ignored on other environments.
+  if (true) {
+    // Check if the visitors IP address is allowed
+    if (allowedIps.includes(ip)) {
+        console.log("DOES NOT INCLUDE");
+      shouldDisallowAccess = false
+    }
+  }
+
+  // Return the 403 status code
+  if (shouldDisallowAccess) {
+    res.statusCode = 403
+    res.end(`Not allowed for |${ip}|`)
+    return
+  }
+  // This object gets injected into the Pages props, if you want to use them
+  return { shouldDisallowAccess, ip }
 }
 
 export default Home
