317384afe6786e90994cb97e10e038728db95b15
john <microrey20@gmail.com>
2022-04-20 07:13:50 -0700

    Cleaning
    - Remove content from Home module css
    - Empty index.html
    - Remove hardcoded strings from index.js
    - Comment out public resources from app.js

--- pages/_app.js
+++ pages/_app.js
@@ -2,26 +2,29 @@
 // import '../public/style-react.css'
 // import '../public/css/components/dark-components.css'
 
-import '../public/MyFontsWebfontsKit.css';
-import '../public/css/bootstrap.css';
-import '../public/style-fixed.css';
-import '../public/css/dark.css';
-import '../public/fonts/css/all.css';
-import '../public/css/font-icons.css';
-import '../public/css/animate.css';
-import '../public/css/magnific-popup-fixed.css';
-import '../public/css/custom.css';
+// import '../public/MyFontsWebfontsKit.css';
+// import '../public/css/bootstrap.css';
+// import '../public/style-fixed.css';
+// import '../public/css/dark.css';
+// import '../public/fonts/css/all.css';
+// import '../public/css/font-icons.css';
+// import '../public/css/animate.css';
+// import '../public/css/magnific-popup-fixed.css';
+// import '../public/css/custom.css';
+
 // import '../public/include/rs-plugin/css/settings.css';
 // <link rel="stylesheet" type="text/css" href="../include/rs-plugin/css/settings.css" media="screen"/>
-import '../public/include/rs-plugin/css/layers.css';
-import '../public/spelll/spelll.css';
-import '../public/spelll/css/fonts.css';
-import '../public/include/rs-plugin/css/addons/revolution.addon.revealer.css';
-import '../public/include/rs-plugin/css/addons/revolution.addon.revealer.preloaders.css';
-import '../public/include/rs-plugin/css/addons/revolution.addon.particles.css';
+
+// import '../public/include/rs-plugin/css/layers.css';
+// import '../public/spelll/spelll.css';
+// import '../public/spelll/css/fonts.css';
+// import '../public/include/rs-plugin/css/addons/revolution.addon.revealer.css';
+// import '../public/include/rs-plugin/css/addons/revolution.addon.revealer.preloaders.css';
+// import '../public/include/rs-plugin/css/addons/revolution.addon.particles.css';
 
 
-import '../styles/spelllbook.css';
+// import '../styles/spelllbook.css';
+
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
--- pages/index.js
+++ pages/index.js
@@ -2,17 +2,20 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
+const title = "Confused Heroes";
+const description = "Confused Heroes Site"
+
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Spelllbook</title>
-        <meta name="description" content="Spelllbook" />
+        <title>{title}</title>
+        <meta name="description" content={description} />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
-        <h1>---== UNDER CONSTRUCTION ==---</h1>
+        <h1>----=== UNDER CONSTRUCTION ===----</h1>
       </main>
 
       <footer className={styles.footer}>
