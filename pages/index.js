7853ece43e3c74c71f6656d825668646e67d75e2
john <microrey20@gmail.com>
2022-04-20 14:06:50 -0700

    Extract whitelist to library

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import useWhitelist from "../lib/useWhitelist"
 
 const title = "Confused Heroes";
 const description = "Confused Heroes Site"
@@ -25,32 +26,9 @@ const Home = props => {
   )
 }
 
-Home.getInitialProps = async ({ req, res }) => {// Disallow access by default
-  let shouldDisallowAccess = true
-
-  // Specify IP's that you want to whitelist. This could be an office/VPN IP address.
-  const allowedIps = ['37.6.11.79', '::ffff:127.0.0.1']
-
-  // Read the request objects headers to find our who's trying to access this page
-  const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress
-
-  // This if statement is only valid if the app is deployed through Now. It gets ignored on other environments.
-  if (true) {
-    // Check if the visitors IP address is allowed
-    if (allowedIps.includes(ip)) {
-        console.log("DOES NOT INCLUDE");
-      shouldDisallowAccess = false
-    }
-  }
-
-  // Return the 403 status code
-  if (shouldDisallowAccess) {
-    res.statusCode = 403
-    res.end(`Not allowed for |${ip}|`)
-    return
-  }
-  // This object gets injected into the Pages props, if you want to use them
-  return { shouldDisallowAccess, ip }
+Home.getInitialProps = async ({req, res}) => {
+    const ipData = useWhitelist(req, res)
+    return ipData
 }
 
 export default Home
