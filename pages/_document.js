1c537e2728a25223660edb11edfafbe82e110ee3
john <microrey20@gmail.com>
2022-04-21 22:50:55 -0700

    Reformat _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,28 +1,28 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document, {Html, Head, Main, NextScript} from 'next/document'
 import React from "react";
 
-  const title = "MINT CONFUSED HEROES"
+const title = "MINT CONFUSED HEROES"
 
 class MyDocument extends Document {
-  static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx)
-    return { ...initialProps }
-  }
+    static async getInitialProps(ctx) {
+        const initialProps = await Document.getInitialProps(ctx)
+        return {...initialProps}
+    }
 
-  render() {
-    return (
-      <Html>
-          <Head>
-              <title>{title}</title>
-              <link rel="shortcut icon" href="/favicon.png" />
-          </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    )
-  }
+    render() {
+        return (
+            <Html>
+                <Head>
+                    <title>{title}</title>
+                    <link rel="shortcut icon" href="/favicon.png"/>
+                </Head>
+                <body>
+                <Main/>
+                <NextScript/>
+                </body>
+            </Html>
+        )
+    }
 }
 
 export default MyDocument
