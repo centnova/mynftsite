639b52b762563a81699012093394f27c20ecc233
john <microrey20@gmail.com>
2022-04-21 22:46:53 -0700

    Add favicon

--- pages/_document.js
+++ pages/_document.js
@@ -11,6 +11,7 @@ class MyDocument extends Document {
     return (
       <Html>
           <Head>
+              <link rel="shortcut icon" href="/favicon.png" />
           </Head>
         <body>
           <Main />
