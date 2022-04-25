df330094731f39c3e5549f88ce9c41785ae6254e
john <microrey20@gmail.com>
2022-04-25 04:54:59 -0700

    Whitelist Mint from proof

--- pages/mint/index.js
+++ pages/mint/index.js
@@ -71,24 +71,55 @@ const Mint = props => {
     try {
 
         const accounts = await provider.getSigner();
+        console.log("Accounts: " + util.inspect(accounts, {showHidden: false, depth: null}));
+
         console.log(contract);
-        console.log("Accounts" + accounts);
+
 
         // recheck if saleIsActive, just in case something changed
         // todo add here also the whitelist case
         let saleIsActive = await contract.saleIsActive();
-        if (!saleIsActive) {
-            console.log("The sale is not active, we will not send a transaction")
+        let wlSaleIsActive = await contract.wlSaleIsActive();
+
+        // todo do an extra check for the tokens here and the type of sale
+        const sendWithSigner = contract.connect(accounts);
+        const weiValue = ethers.utils.parseEther(totalValue.toString());
+        let mintedWith = "";
+
+        if (wlSaleIsActive) {
+            let proofs = [];
+            let waladdress = await accounts.getAddress();
+            console.log("WALADDRESS" + waladdress);
+
+            // [{'address': 'AB5Dae357BdBd6F7873ddC9741022d8c90a890e4', 'leaf': '25982080fcd47bab8c4177e3da42a49c461dea32dc9b6973bb5350c05fccd774', 'proof': ['80bc166688d1b9b7144865894626695cbc86cff46fc239d4e4ef9f63fda2f4db']}, {'address': '506A3b92e6D80e6aCb7106a70CDd59A58D588491', 'leaf': '80bc166688d1b9b7144865894626695cbc86cff46fc239d4e4ef9f63fda2f4db', 'proof': ['25982080fcd47bab8c4177e3da42a49c461dea32dc9b6973bb5350c05fccd774']}]
+            if (waladdress === '0xAB5Dae357BdBd6F7873ddC9741022d8c90a890e4')
+            {
+                proofs = ['0x80bc166688d1b9b7144865894626695cbc86cff46fc239d4e4ef9f63fda2f4db'];
+                console.log("Wallet address: " + waladdress);
+            }
+            else if (waladdress === '0x506A3b92e6D80e6aCb7106a70CDd59A58D588491') {
+                proofs = ['0x25982080fcd47bab8c4177e3da42a49c461dea32dc9b6973bb5350c05fccd774'];
+                console.log("Wallet address: " + waladdress);
+            } else {
+                console.log("NO PROOFS - NOT WHITELISTED");
+                return
+            }
+
+
+            await sendWithSigner.wlMintToken(tokens, proofs, {value: weiValue});
+            mintedWith = "Whitelist";
+        }
+        else if (saleIsActive) {
+            await sendWithSigner.mintToken(tokens, {value: weiValue});
+            mintedWith = "Public Sale";
+        }
+        else {
+            console.log("Neither sale is active, we will not send a transaction");
             return
         }
 
-        const sendWithSigner = contract.connect(accounts);
-
-        const weiValue = ethers.utils.parseEther(totalValue.toString());
         console.log(weiValue);
-
-        await sendWithSigner.mintToken(tokens, {value: weiValue});
-        console.log(`Minted ${tokens} for a total of ${totalValue}`);
+        console.log(`Minted ${tokens} with ${mintedWith} for a total of ${totalValue}`);
 
         // Router.pushRoute('/');
     } catch (err) {
