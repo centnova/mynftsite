17aea884482ae1a70f64b8f1807df4c71c657691
john <microrey20@gmail.com>
2022-04-25 07:23:06 -0700

    Reformat Code

--- pages/mint/index.js
+++ pages/mint/index.js
@@ -23,12 +23,10 @@ const Mint = props => {
     if (props.wlSaleIsActive) {
         maxTokens = 4;
         saleMessage = `WHITELIST MINT. YOU CAN MINT UP TO ${maxTokens}.`;
-    }
-    else if (props.saleIsActive) {
+    } else if (props.saleIsActive) {
         maxTokens = 8;
         saleMessage = `PUBLIC MINT. YOU CAN MINT UP TO ${maxTokens} PER TRANSACTION`;
-    }
-    else {
+    } else {
         saleMessage = `MINT IS NOT OPEN`;
     }
 
@@ -66,71 +64,67 @@ const Mint = props => {
     }
 
     const onSubmit = async (event) => {
-    setErrorMessage('');
-    event.preventDefault();
-    setLoading(true);
-    try {
+        setErrorMessage('');
+        event.preventDefault();
+        setLoading(true);
+        try {
 
-        const accounts = await provider.getSigner();
-        console.log("Accounts: " + util.inspect(accounts, {showHidden: false, depth: null}));
+            const accounts = await provider.getSigner();
+            console.log("Accounts: " + util.inspect(accounts, {showHidden: false, depth: null}));
 
-        console.log(contract);
+            console.log(contract);
 
 
-        // recheck if saleIsActive, just in case something changed
-        // todo add here also the whitelist case
-        let saleIsActive = await contract.saleIsActive();
-        let wlSaleIsActive = await contract.wlSaleIsActive();
+            // recheck if saleIsActive, just in case something changed
+            // todo add here also the whitelist case
+            let saleIsActive = await contract.saleIsActive();
+            let wlSaleIsActive = await contract.wlSaleIsActive();
 
-        // todo do an extra check for the tokens here and the type of sale
-        const sendWithSigner = contract.connect(accounts);
-        const weiValue = ethers.utils.parseEther(totalValue.toString());
-        let mintedWith = "";
+            // todo do an extra check for the tokens here and the type of sale
+            const sendWithSigner = contract.connect(accounts);
+            const weiValue = ethers.utils.parseEther(totalValue.toString());
+            let mintedWith = "";
 
-        if (wlSaleIsActive) {
-            let proofs = [];
-            let waladdress = await accounts.getAddress();
-            console.log("WALADDRESS" + waladdress);
+            if (wlSaleIsActive) {
+                let proofs = [];
+                let waladdress = await accounts.getAddress();
+                console.log("WALADDRESS" + waladdress);
 
-            // [{'address': 'AB5Dae357BdBd6F7873ddC9741022d8c90a890e4', 'leaf': '25982080fcd47bab8c4177e3da42a49c461dea32dc9b6973bb5350c05fccd774', 'proof': ['80bc166688d1b9b7144865894626695cbc86cff46fc239d4e4ef9f63fda2f4db']}, {'address': '506A3b92e6D80e6aCb7106a70CDd59A58D588491', 'leaf': '80bc166688d1b9b7144865894626695cbc86cff46fc239d4e4ef9f63fda2f4db', 'proof': ['25982080fcd47bab8c4177e3da42a49c461dea32dc9b6973bb5350c05fccd774']}]
-            if (waladdress === '0xAB5Dae357BdBd6F7873ddC9741022d8c90a890e4')
-            {
-                proofs = ['0x80bc166688d1b9b7144865894626695cbc86cff46fc239d4e4ef9f63fda2f4db'];
-                console.log("Wallet address: " + waladdress);
-            }
-            else if (waladdress === '0x506A3b92e6D80e6aCb7106a70CDd59A58D588491') {
-                proofs = ['0x25982080fcd47bab8c4177e3da42a49c461dea32dc9b6973bb5350c05fccd774'];
-                console.log("Wallet address: " + waladdress);
+                // [{'address': 'AB5Dae357BdBd6F7873ddC9741022d8c90a890e4', 'leaf': '25982080fcd47bab8c4177e3da42a49c461dea32dc9b6973bb5350c05fccd774', 'proof': ['80bc166688d1b9b7144865894626695cbc86cff46fc239d4e4ef9f63fda2f4db']}, {'address': '506A3b92e6D80e6aCb7106a70CDd59A58D588491', 'leaf': '80bc166688d1b9b7144865894626695cbc86cff46fc239d4e4ef9f63fda2f4db', 'proof': ['25982080fcd47bab8c4177e3da42a49c461dea32dc9b6973bb5350c05fccd774']}]
+                if (waladdress === '0xAB5Dae357BdBd6F7873ddC9741022d8c90a890e4') {
+                    proofs = ['0x80bc166688d1b9b7144865894626695cbc86cff46fc239d4e4ef9f63fda2f4db'];
+                    console.log("Wallet address: " + waladdress);
+                } else if (waladdress === '0x506A3b92e6D80e6aCb7106a70CDd59A58D588491') {
+                    proofs = ['0x25982080fcd47bab8c4177e3da42a49c461dea32dc9b6973bb5350c05fccd774'];
+                    console.log("Wallet address: " + waladdress);
+                } else {
+                    console.log("NO PROOFS - NOT WHITELISTED");
+                    return
+                }
+
+
+                await sendWithSigner.wlMintToken(tokens, proofs, {value: weiValue});
+                mintedWith = "Whitelist";
+            } else if (saleIsActive) {
+                await sendWithSigner.mintToken(tokens, {value: weiValue});
+                mintedWith = "Public Sale";
             } else {
-                console.log("NO PROOFS - NOT WHITELISTED");
+                console.log("Neither sale is active, we will not send a transaction");
                 return
             }
 
+            console.log(weiValue);
+            console.log(`Minted ${tokens} with ${mintedWith} for a total of ${totalValue}`);
 
-            await sendWithSigner.wlMintToken(tokens, proofs, {value: weiValue});
-            mintedWith = "Whitelist";
+            // Router.pushRoute('/');
+        } catch (err) {
+            setErrorMessage(err.message);
+            console.log(err.message);
         }
-        else if (saleIsActive) {
-            await sendWithSigner.mintToken(tokens, {value: weiValue});
-            mintedWith = "Public Sale";
-        }
-        else {
-            console.log("Neither sale is active, we will not send a transaction");
-            return
-        }
+        setLoading(false);
+    };
 
-        console.log(weiValue);
-        console.log(`Minted ${tokens} with ${mintedWith} for a total of ${totalValue}`);
-
-        // Router.pushRoute('/');
-    } catch (err) {
-        setErrorMessage(err.message);
-        console.log(err.message);
-    }
-    setLoading(false);
-};
-
-    return(
+    return (
         <div className={styles.main}>
             <style jsx global>{`
         html {
@@ -142,35 +136,30 @@ const Mint = props => {
 }
       `}</style>
             <img src="img/logomain.png" alt="Confused Heroes" width="240"/> <br/> <br/>
-                <div className={styles.heads}>MINT CONFUSED HEROES <br/></div>
-                <br/>
-                    <div className={styles.under}></div>
-                    <br/>
-<ChMintForm
-    saleMessage={saleMessage}
-    totalValue={totalValue}
-    tokens={tokens}
-    decreaseTokens={decreaseTokens}
-    increaseTokens={increaseTokens}
-    onSubmit={onSubmit}
-    totalSupply={props.totalSupply}
-    maxSupply={props.maxSupply}
-    loading={loading}
-></ChMintForm>
+            <div className={styles.heads}>MINT CONFUSED HEROES <br/></div>
+            <br/>
+            <div className={styles.under}></div>
+            <br/>
+            <ChMintForm
+                saleMessage={saleMessage}
+                totalValue={totalValue}
+                tokens={tokens}
+                decreaseTokens={decreaseTokens}
+                increaseTokens={increaseTokens}
+                onSubmit={onSubmit}
+                totalSupply={props.totalSupply}
+                maxSupply={props.maxSupply}
+                loading={loading}
+            ></ChMintForm>
 
 
-
-
-
-
-
-                                                    <br/>
+            <br/>
 
         </div>
     )
 }
 
-export const getServerSideProps = async({req, res}) => {
+export const getServerSideProps = async ({req, res}) => {
     console.log("Inside Server Props");
     const ipData = useWhitelist(req, res)
 
